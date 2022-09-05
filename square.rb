db354f73affe951ceeb1f10661425b6b9e838132
Unhyphenated <julian.jong75@protonmail.com>
2022-09-05 15:59:54 +0800

    Added hide and reveal class methods.

--- square.rb
+++ square.rb
@@ -1,5 +1,26 @@
 class Square
-    def initialize(empty=true)
-        
+    attr_reader :value
+
+    def initialize(value, revealed=false)
+        @value = value
+        @revealed = revealed
     end
+
+    def hide
+        @revealed = false
+    end
+
+    def reveal
+        @revealed = true
+    end
+
+    def revealed?
+        @revealed == true 
+    end
+    
+    def to_s
+        @revealed ? value.to_s : "_"
+    end
+
+
 end
