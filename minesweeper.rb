bd34332f02f6c07b5699721b61bda140c169b50c
Unhyphenated <julian.jong75@protonmail.com>
2022-09-06 15:46:05 +0800

    Added prompt.

--- minesweeper.rb
+++ minesweeper.rb
@@ -5,6 +5,10 @@ class Minesweeper
     end
 
     def prompt
-        ""
+        puts "Please enter a position you'd like to uncover (e.g., 2, 1)."
+        puts "Add 'F' after the position to flag it (e.g., 2, 1, F)."
+        p "> "
     end
+
+    
 end
